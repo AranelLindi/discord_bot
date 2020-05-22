5d16bc64f982f4c882ce6dec315e98a768392389
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-05-22 21:22:07 +0200

    Erster kleiner funktionsfähiger Bot! :)

--- mybot.py
+++ mybot.py
@@ -1,6 +1,10 @@
+import os
+
 import discord
 
+
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
@@ -12,14 +16,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    if message.content.startswith('Hallo'):
+        await message.channel.send(f'Hallo {message.author}!')
+        #await message.channel.send(message.author)
+
+    if message.content.startswith('Tutorial'):
+        await message.channel.send("Hallo Welt!")
 
 # Discord Token darf nicht im Internet veröffentlicht werden. Daher:
-# den Token in einem seperatem File speichern und diese in die .gitignore
+# das Token in einem seperatem File speichern und diese in die .gitignore
 # Datei aufnehmen, sodass keine Veröffentlichung erfolgt.
 
-file_object = open( "token.txt", "r" )
+file_object = open( ".token", "r" )
 
 token = file_object.read()
 
